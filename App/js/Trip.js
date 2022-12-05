9cd32b579bce5c93a2a9476721f47b5b0da0b1c4
miodraggosic <gosmio88@gmail.com>
2022-12-05 23:55:54 +0100

    class Trip added

---
+++ App/js/Trip.js
@@ -0,0 +1,10 @@
+class Trip {
+  constructor(title, url, description, fromDate, toDate, country) {
+    this.title = title;
+    this.imageUrl = url;
+    this.description = description;
+    this.fromDate = fromDate;
+    this.toDate = toDate;
+    this.country = country;
+  }
+}
