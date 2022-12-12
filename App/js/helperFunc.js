b95e3e300b28b88ed3f1b0c6db46db0bf702db14
miodraggosic <gosmio88@gmail.com>
2022-12-13 00:21:36 +0100

    extracted helper func, not implemented

---
+++ App/js/helperFunc.js
@@ -0,0 +1,84 @@
+function createElem(el) {
+  return document.createElement(el);
+}
+
+function reverseDate(date) {
+  return date.split("-").reverse().join("-");
+}
+
+function tripCard(obj) {
+  const divCard = createElem("div");
+  divCard.classList.add("card");
+
+  const title = createElem("a");
+  title.textContent = obj.title;
+  title.href = `./pages/tripOverview.html?id=${obj.id}`;
+
+  const country = createElem("h4");
+  country.textContent = obj.country;
+
+  const img = createElem("img"); //insert as background image
+  img.src = obj.imageUrl;
+
+  const fromDate = createElem("span");
+  fromDate.textContent = reverseDate(obj.fromDate);
+
+  const toDate = createElem("span");
+  toDate.textContent = reverseDate(obj.toDate);
+
+  const description = createElem("h6");
+  description.textContent = obj.description;
+
+  const readMore = createElem("button");
+  const price = createElem("h3");
+  price.textContent = obj.price;
+
+  divCard.append(
+    title,
+    country,
+    img,
+    fromDate,
+    toDate,
+    description,
+    readMore,
+    price
+  );
+  return divCard;
+}
+
+function fullTripCard(obj) {
+  const divCard = createElem("div");
+  divCard.classList.add("fullCard");
+
+  const imgWrapper = createElem("div");
+  imgWrapper.classList.add("imgWrapper");
+  console.log(imgWrapper);
+  imgWrapper.style.backgroundImage = `url("${obj.imageUrl}")`;
+
+  const textWrapper = createElem("div");
+  textWrapper.classList.add("textWrapper");
+
+  const country = createElem("h3");
+  country.textContent = obj.country;
+
+  const title = createElem("h1");
+  title.textContent = obj.title;
+
+  const fromDate = createElem("span");
+  fromDate.textContent = reverseDate(obj.fromDate);
+
+  const toDate = createElem("span");
+  toDate.textContent = reverseDate(obj.toDate);
+
+  const description = createElem("h4");
+  description.textContent = obj.description;
+
+  const price = createElem("h3");
+  price.textContent = obj.price;
+
+  textWrapper.append(country, title, fromDate, toDate, description, price);
+
+  divCard.append(imgWrapper, textWrapper);
+
+  return divCard;
+}
