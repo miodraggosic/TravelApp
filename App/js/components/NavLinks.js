09c26d6095a36cc9f92c6b8a4855e086003fca95
miodraggosic <gosmio88@gmail.com>
2022-12-13 00:20:37 +0100

    extracted as component, not implemented

---
+++ App/js/components/NavLinks.js
@@ -0,0 +1,24 @@
+class NavLinks {
+  constructor() {
+    this.links = [
+      { name: "Home", path: "../index.html" },
+      { name: "Admin", path: "../pages/admin.html" },
+      { name: "Login", path: "#" },
+      { name: "Logout", path: "#" },
+    ];
+  }
+
+  createLink(obj) {
+    const aTag = createElem("a");
+    aTag.textContent = obj.name;
+    aTag.classList.add("navLink");
+    aTag.href = obj.path;
+    return aTag;
+  }
+
+  renderLinks(elem) {
+    for (const link of this.links) {
+      elem.appendChild(this.createLink(link));
+    }
+  }
+}
