f6a77a03335a9ea15335d38a1f6555861b2d35a3
miodraggosic <gosmio88@gmail.com>
2022-12-16 02:10:53 +0100

    tableTrips modified, createTripBtn add & redirect

--- App/js/admin.js
+++ App/js/admin.js
@@ -5,26 +5,54 @@ import Api from "./env.js";
 
 const displayLinks = document.querySelector(".navLinks");
 const displayTableTrips = document.querySelector(".tableTrips");
+const createTripBtn = document.getElementById("createTrip");
+
+const navLinks = new NavLinks();
+navLinks.renderLinks(displayLinks);
 
 const trips = new Trips(Api);
 trips.getTrips(null, displayTableTrips, tableTrips);
 
-const navLinks = new NavLinks();
-navLinks.renderLinks(displayLinks);
+createTripBtn.addEventListener("click", () => {
+  window.location.href = "./createTrip.html";
+});
 
 function tableTrips(obj) {
   const createRow = (obj) => {
     const tr = helper.createElem("tr");
 
-    const objValues = Object.values(obj);
-    const objId = objValues.pop();
-    objValues.unshift(objId);
+    const tdIndex = helper.createElem("td");
+    tdIndex.textContent = obj.id;
 
-    objValues.forEach((value) => {
-      const td = helper.createElem("td");
-      td.textContent = value;
-      tr.appendChild(td);
-    });
+    const tdImage = helper.createElem("td");
+    tdImage.classList.add("tdImage");
+    tdImage.style.backgroundImage = `url("${obj.imageUrl}")`;
+
+    const tdTitle = helper.createElem("td");
+    tdTitle.textContent = obj.title;
+
+    const tdCountry = helper.createElem("td");
+    tdCountry.textContent = obj.country;
+
+    const tdPrice = helper.createElem("td");
+    tdPrice.textContent = obj.price;
+    tdPrice.classList.add("price");
+
+    const tdDateFrom = helper.createElem("td");
+    tdDateFrom.textContent = helper.reverseDate(obj.fromDate);
+
+    const tdDateTo = helper.createElem("td");
+    tdDateTo.textContent = helper.reverseDate(obj.toDate);
+
+    tr.append(
+      tdIndex,
+      tdImage,
+      tdTitle,
+      tdCountry,
+      tdPrice,
+      tdDateFrom,
+      tdDateTo
+    );
     return tr;
   };
 
