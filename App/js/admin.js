f52180c5563bf66617a1671ec7445ddd527cd030
miodraggosic <gosmio88@gmail.com>
2022-12-17 07:54:12 +0100

    tableTrips extracted to helper

--- App/js/admin.js
+++ App/js/admin.js
@@ -11,65 +11,8 @@ const navLinks = new NavLinks();
 navLinks.renderLinks(displayLinks);
 
 const trips = new Trips(Api);
-trips.getTrips(null, displayTableTrips, tableTrips);
+trips.getTrips(null, displayTableTrips, helper.tableTrips);
 
 createTripBtn.addEventListener("click", () => {
   window.location.href = "./createTrip.html";
 });
-
-function tableTrips(obj) {
-  const createRow = (obj) => {
-    const tr = helper.createElem("tr");
-
-    const tdIndex = helper.createElem("td");
-    tdIndex.textContent = obj.id;
-
-    const tdImage = helper.createElem("td");
-    tdImage.classList.add("tdImage");
-    tdImage.style.backgroundImage = `url("${obj.imageUrl}")`;
-
-    const tdTitle = helper.createElem("td");
-    tdTitle.textContent = obj.title;
-
-    const tdCountry = helper.createElem("td");
-    tdCountry.textContent = obj.country;
-
-    const tdPrice = helper.createElem("td");
-    tdPrice.textContent = obj.price;
-    tdPrice.classList.add("price");
-
-    const tdDateFrom = helper.createElem("td");
-    tdDateFrom.textContent = helper.reverseDate(obj.fromDate);
-
-    const tdDateTo = helper.createElem("td");
-    tdDateTo.textContent = helper.reverseDate(obj.toDate);
-
-    tr.append(
-      tdIndex,
-      tdImage,
-      tdTitle,
-      tdCountry,
-      tdPrice,
-      tdDateFrom,
-      tdDateTo
-    );
-    return tr;
-  };
-
-  const createEditBtn = (id) => {
-    const editTd = helper.createElem("td");
-    const editBtn = helper.createElem("button");
-
-    editBtn.setAttribute("data-id", id);
-    editBtn.textContent = "Edit";
-
-    editTd.appendChild(editBtn);
-    return editTd;
-  };
-
-  const tr = createRow(obj);
-  const editBtn = createEditBtn(obj.id);
-
-  tr.appendChild(editBtn);
-  return tr;
-}
